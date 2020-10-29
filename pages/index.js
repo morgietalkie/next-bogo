2538d818439be39e03cc5058fd7672f5d9f0a9ba
Morgan Grage Rasmussen <morgangr01@gmail.com>
2020-10-29 22:16:43 +0100

    update page title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Well done</title>
+        <title>B&o test site</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <main className="contentIndex">
